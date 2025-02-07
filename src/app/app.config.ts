084f43fca83a43f2fa591792932d6193347f2b67
Jakson Britzke <jaksonbritzke@gmail.com>
2025-02-07 20:26:30 -0300

    Adicionado Tema Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import Aura from '@primeng/themes/aura';
-import { providePrimeNG } from 'primeng/config';
+import Material from '@primeng/themes/material';
 import { routes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-          preset: Aura
+          preset: Material
       }
   }),
     provideZoneChangeDetection({ eventCoalescing: true }),
